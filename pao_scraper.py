3119fd3d5a13531b84afe0b37a5409bdfd40e9e6
RhamnusiaCdg <rhamnusiacdg@gmail.com>
2026-01-10 15:33:01 +0200

    Fix: Disable browser for GitHub Actions

--- pao_scraper.py
+++ pao_scraper.py
@@ -39,7 +39,7 @@ def authenticate_google_calendar():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=0, open_browser=False)
         
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
